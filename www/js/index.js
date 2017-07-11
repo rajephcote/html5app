b8cb04c8bebd5e2297f2701114bec65f77901ee9
rajephcote <rjephcote@skynoodle.co.uk>
2017-07-11 14:16:43 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var mainurl = 'http://www.kimhauser.ch';
+var mainurl = 'http://expertlms.training';
 
 var app = {
 
